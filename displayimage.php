54996f45a416157e908c5e73dac5b474b6987d72
Sydney Hardin <Syd@syd.local>
2011-11-29 20:37:45 -0500

    A bunch of stuff

--- displayimage.php
+++ displayimage.php
@@ -101,3 +101,17 @@ $image_text_width = $image_text_size_array[0];
 
       </tr>
     </table>
+    
+    <script type="text/javascript">
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-27352681-1']);
+			_gaq.push(['_trackPageview']);
+		
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		
+		</script>
--- index.php
+++ index.php
@@ -95,23 +95,37 @@ include "head.php";
             </tr>
             <tr>
               <td>
-			  	<a href="http://www.facebook.com/pages/Sydney-Phillips-Hardin/28237807133">
-			     <img
-			       style="border: 0; width: 25px; height: 25px;"
-			       alt="FACEBOOK"
-			       src="facebook-icon.png"
-			     /><img
-			       style="border: 0; width: 230px; height: 22px;"
-			       alt="LIKE"
-			       src="text/like.gif"
-			     />	
-                </a>
+								<a href="http://www.facebook.com/pages/Sydney-Phillips-Hardin/28237807133">
+									<img
+										style="border: 0; width: 25px; height: 25px; margin-right: 5px;"
+										alt="FACEBOOK"
+										src="facebook-icon.png"
+									/></a><a href="http://www.flickr.com/photos/pensivewombat/"><img
+										style="border: 0; width: 25px; height: 25px;"
+										alt="flickr"
+										src="flickr_icon.png"
+									/>
+								</a>
               </td>
             </tr>
           </table>
         </td>
       </tr>
     </table>
+    
+    <script type="text/javascript">
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-27352681-1']);
+			_gaq.push(['_trackPageview']);
+		
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		
+		</script>
     
     
   </body>
